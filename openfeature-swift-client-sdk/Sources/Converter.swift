6cc861f9a8328547a142927018f79292d4cace5f
duyhungtnn <duyhung71089@gmail.com>
2025-01-12 16:28:10 +0700

    feat: init provider

---
+++ openfeature-swift-client-sdk/Sources/Converter.swift
@@ -0,0 +1,72 @@
+import Bucketeer
+import OpenFeature
+
+extension BKTEvaluationDetails where T == BKTValue {
+    func toProviderEvaluation(overrideValue: Value? = nil) -> ProviderEvaluation<Value> {
+        return ProviderEvaluation<Value>(
+            value: overrideValue ?? self.variationValue.toOpenFeatureValue(),
+            flagMetadata: [:],
+            variant: nil,
+            reason: self.reason.rawValue)
+    }
+}
+
+extension BKTEvaluationDetails where T: Equatable {
+    func toProviderEvaluation() -> ProviderEvaluation<T> {
+        return ProviderEvaluation<T>(
+            value: self.variationValue,
+            flagMetadata: [:],
+            variant: nil,
+            reason: self.reason.rawValue)
+    }
+}
+
+
+extension BKTValue {
+    func toOpenFeatureValue() -> Value {
+        switch self {
+        case .boolean(let raw):
+            return .boolean(raw)
+        case .string(let raw):
+            return .string(raw)
+        case .number(let raw):
+            return .double(raw)
+        case .list(let raw):
+            return .list(raw.map({ $0.toOpenFeatureValue()}))
+        case .dictionary(let raw):
+            return .structure(raw.mapValues({ $0.toOpenFeatureValue()}))
+        case .null:
+            return .null
+        }
+    }
+}
+
+extension Value {
+    func toBKTValue() -> BKTValue {
+        switch self {
+        case .boolean(let raw):
+            return .boolean(raw)
+        case .string(let raw):
+            return .string(raw)
+        case .double(let raw):
+            return .number(raw)
+        case .list(let raw):
+            return .list(raw.map({ value in
+                return value.toBKTValue()
+            }))
+        case .structure(let raw):
+            var convertedValue: [String:BKTValue] = [:]
+            
+            raw.forEach { key, value in
+                convertedValue[key]=value.toBKTValue()
+            }
+            return .dictionary(convertedValue)
+        case .null:
+            return .null
+        case .integer(let raw):
+            return .number(Double(raw))
+        case .date(let raw):
+            return .number(raw.timeIntervalSince1970)
+        }
+    }
+}
---
+++ openfeature-swift-client-sdk/Sources/Provider.swift
@@ -0,0 +1,95 @@
+import Bucketeer
+import OpenFeature
+import Combine
+
+class BucketeerProvider: FeatureProvider {
+    private let eventHandler = EventHandler(ProviderEvent.notReady)
+    var hooks: [any Hook] = []
+    var metadata: ProviderMetadata = Metadata()
+    
+    private let config: BKTConfig
+    private let user: BKTUser
+    
+    public init(
+        config: BKTConfig,
+        user: BKTUser
+    ) {
+        self.config = config
+        self.user = user
+    }
+    
+    /// Called by OpenFeatureAPI whenever the new Provider is registered
+    func initialize(initialContext: (any EvaluationContext)?) {
+        do {
+            try BKTClient.initialize(
+                config: config,
+                user: user
+            ) { error in
+                if let error = error {
+                    if case .timeout = error {
+                        self.eventHandler.send(.ready)
+                    } else {
+                        self.eventHandler.send(.error)
+                    }
+                    return
+                }
+                self.eventHandler.send(.ready)
+            }
+            
+        } catch {
+            // OpenFeatureError .providerFatarError(message: error.localizedDescription
+            self.eventHandler.send(.error)
+        }
+    }
+    
+    func onContextSet(oldContext: (any EvaluationContext)?, newContext: any EvaluationContext) {
+        
+    }
+    
+    func getBooleanEvaluation(key: String, defaultValue: Bool, context: (any EvaluationContext)?) throws -> ProviderEvaluation<Bool> {
+        let client = try BKTClient.shared
+        let evaluationDetails = client.boolVariationDetails(featureId: key, defaultValue: defaultValue)
+        return evaluationDetails.toProviderEvaluation()
+    }
+    
+    func getStringEvaluation(key: String, defaultValue: String, context: (any EvaluationContext)?) throws -> ProviderEvaluation<String> {
+        let client = try BKTClient.shared
+        let evaluationDetails = client.stringVariationDetails(featureId: key, defaultValue: defaultValue)
+        return evaluationDetails.toProviderEvaluation()
+    }
+    
+    func getIntegerEvaluation(key: String, defaultValue: Int64, context: (any EvaluationContext)?) throws -> ProviderEvaluation<Int64> {
+        throw CancellationError()
+    }
+    
+    func getDoubleEvaluation(key: String, defaultValue: Double, context: (any EvaluationContext)?) throws -> ProviderEvaluation<Double> {
+        let client = try BKTClient.shared
+        let evaluationDetails = client.doubleVariationDetails(featureId: key, defaultValue: defaultValue)
+        return evaluationDetails.toProviderEvaluation()
+    }
+    
+    func getObjectEvaluation(key: String, defaultValue: Value, context: (any EvaluationContext)?) throws -> ProviderEvaluation<Value> {
+        let client = try BKTClient.shared
+        let bktDefaultValue = defaultValue.toBKTValue()
+        let evaluationDetails = client.objectVariationDetails(featureId: key, defaultValue: bktDefaultValue)
+        let shouldUseOpenFeatureDefaultValue = bktDefaultValue == evaluationDetails.variationValue
+        // If the value is the same as the default value, it indicates that the feature flag was not found,
+        // and the default value was used. In this case, we should return the default value to OpenFeature.
+
+        // Why we need overrideValue: there are differences between BKTValue and OpenFeatureValue.
+        // BKTValue does not support .int64 and .date types, so we need to convert the BKTValue to Value.
+        // However, for the default value, we can directly return it without additional conversion.
+        return evaluationDetails.toProviderEvaluation(
+            overrideValue: shouldUseOpenFeatureDefaultValue ? defaultValue : nil
+        )
+    }
+    
+    func observe() -> AnyPublisher<ProviderEvent, Never> {
+        return eventHandler.observe()
+    }
+}
+
+struct Metadata: ProviderMetadata {
+    var name: String? = "Bucketeer Feature Flag Provider"
+}
+
